bb142222d15b793287b460539fa3d69e663cb34f
Dmitry Belousov <belavusdm@gmail.com>
2025-07-14 00:09:47 +0300

    Add GetGame endpoint

--- src/API/Controllers/GamesController.cs
+++ src/API/Controllers/GamesController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.DTOs;
 using Application.UseCases.CreateGame;
+using Application.UseCases.GetGame;
 using Infrastructure.Configuration.Options;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -21,5 +22,19 @@ public class GamesController(IMediator mediator) : ControllerBase
         var game = await mediator.Send(command);
         
         return CreatedAtAction(nameof(CreateGame), new { id = game.Id }, game);
+    }
+    
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(GameDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetGame(Guid id)
+    {
+        var query = new GetGameQuery(id);
+        
+        var game = await mediator.Send(query);
+        
+        Response.Headers.ETag = $"W/\"{game.Version}\"";
+        
+        return Ok(game);
     }
 }
